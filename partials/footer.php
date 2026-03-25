fa31a050175ad4bbe383f07231f0d2086346b678
Herasi Silva <210443293+Hersil23@users.noreply.github.com>
2026-03-25 15:36:26 -0400

    Footer: quitar columna contacto, agregar email info@pintapeg.com en siguenos

--- partials/footer.php
+++ partials/footer.php
@@ -18,28 +18,10 @@
       </ul>
     </div>
 
-    <!-- Columna 3: Contacto -->
-    <div class="footer-col">
-      <h4 class="footer-titulo">Contacto</h4>
-      <ul class="footer-links">
-        <li>
-          <span class="footer-icon">&#128222;</span>
-          <a href="https://wa.me/584265196026" target="_blank">0426-5196026</a>
-        </li>
-        <li>
-          <span class="footer-icon">&#9993;</span>
-          <a href="mailto:mpintapeg@gmail.com">mpintapeg@gmail.com</a>
-        </li>
-        <li>
-          <span class="footer-icon">&#128205;</span>
-          <span>Barquisimeto, Venezuela</span>
-        </li>
-      </ul>
-    </div>
-
-    <!-- Columna 4: Redes sociales -->
+    <!-- Columna 3: Redes sociales -->
     <div class="footer-col footer-redes">
       <h4 class="footer-titulo">Siguenos</h4>
+      <a href="mailto:info@pintapeg.com" class="footer-email">&#9993; info@pintapeg.com</a>
       <div class="footer-social">
         <a href="https://www.instagram.com/pintapeg?igsh=MW8xY2ppN3Y0MmxsMw==" target="_blank" class="social-icon social-instagram" title="Instagram">
           <svg viewBox="0 0 24 24" fill="currentColor" width="22" height="22"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
